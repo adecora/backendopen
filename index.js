5f85927d095f331f27634220c0d48886c8c85b52
Alex de Cora <alexinhocora@gmail.com>
2021-11-09 18:06:01 +0100

    part3-exercise3.1(step1)

--- index.js
+++ index.js
@@ -3,9 +3,37 @@ const app = express();
 
 const PORT = 3001;
 
+const persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+    },
+];
+
 app.get("/", (request, response) => {
-    response.send("<h1>Hello world from express</h1>");
-})
+    response.send("<h1>Phonebook REST API</h1>");
+});
+
+app.get("/api/persons", (request, response) => {
+    response.json(persons);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server listen on port ${PORT}`);
